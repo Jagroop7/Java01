2b7c50c962a4ff2bfc0e5c2b55e1c5240bc3e448
Jagroop7 <er.jagroopkaursandhu93@gmail.com>
2023-02-09 22:47:42 -0500

    updated java Assignment1

--- src/RectPattern.java
+++ src/RectPattern.java
@@ -1,8 +1,11 @@
 public class RectPattern {
     public static void main(String args[]) {
+        int i=1;
         //outer loop
         // SOLID RECTANGLE
-        for(int i = 1; i <= 5; i++) {
+       // for(int i = 1; i <= 5; i++) {
+        while(i <=5){
+            i++;
         //inner loop
             for(int j= 1; j <= 5; j++){
               System.out.print("*");
